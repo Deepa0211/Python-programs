0c1273038da745aa74b7fed763628a8ad4a2f34c
Deepa0211 <deepa.menon.786@gmail.com>
2023-10-31 19:31:07 +0530

    Modified Code

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,12 @@
 import random
+
+# for random number 
 jackpot = random.randint(1, 100)
+
+# for counting no. of attempts
 counter = 1
 
+# User input
 guess = int(input("Guess It "))
 
 while guess != jackpot:
