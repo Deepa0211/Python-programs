46ed0c3d87abd726a87abf81e0adc8c21bbd88f4
Deepa0211 <deepa.menon.786@gmail.com>
2023-10-31 19:29:08 +0530

    Modified Code

--- login_using_ifelse.py
+++ login_using_ifelse.py
@@ -2,14 +2,20 @@
 # Correct password - 12345
 
 email = input("Enter your email: ")
+
+# checking if email contain @
 if "@" in email:
     password = input("Enter your password: ")
 
+    # both correct entries
     if email == "classroom@gmail.com" and password == "12345":
         print("Welcome")
+    # password incorrect
     elif email == "classroom@gmail.com" and password != "12345":
         print("Wrong password")
+        # one more chance
         password = input("Try Again ")
+        # finally correct entry
         if password == "12345":
             print("Finally correct")
         else:
