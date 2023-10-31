a7954392b7891e7a21dd871d0ff303aa298877bb
Deepa0211 <deepa.menon.786@gmail.com>
2023-10-31 19:25:20 +0530

    Modified Code

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -2,4 +2,5 @@
 first_number = int(input("Enter first number:"))
 second_number = int(input("Enter second number:"))
 
+# Adding two numbers
 print("Addition of two numbers:", first_number + second_number)
