bb7bb84065eb365bb86a90f8a09a3e1b39f01012
Deepa0211 <deepa.menon.786@gmail.com>
2023-11-01 19:47:24 +0530

    Modified Code

--- title_function_code.py
+++ title_function_code.py
@@ -2,6 +2,13 @@ sentence = "how are you?".split()
 
 print(sentence)
 
+list = []
+
 for i in sentence:
-    print(i.capitalize(), end=" ")
+    print(i.capitalize())
+    list.append(i.capitalize())
+
+print(list)
+
+print(" ".join(list))
     
